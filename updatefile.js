a726d0773bd4b737c4453f22573731707dcb8dcb
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-09 16:50:24 -0500

    first commit.

---
+++ updatefile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var update = require('update-contributors');
+var extend = require('extend-shallow');
+
+module.exports = function (app, base, env) {
+  // var logger = app.logger('update-contributors');
+
+  // app.task('default', function (cb) {
+  //   try {
+  //     var pkg = base.getFile('package.json');
+  //     var data = extend({}, pkg.json);
+  //     // logger('done');
+  //     update(data, function (err, results) {
+  //       if (err) return cb(err);
+  //     console.log(results)
+  //       pkg.json = results;
+  //       cb();
+  //     });
+  //   } catch (err) {
+  //     cb(err);
+  //   }
+  // });
+};
