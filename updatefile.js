1a88c06c19f4947ceb13c5eefbfdc67f934c74c0
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-31 23:02:18 -0400

    first implementation

--- updatefile.js
+++ updatefile.js
@@ -1,24 +1,51 @@
 'use strict';
 
-var update = require('update-contributors');
-var extend = require('extend-shallow');
+var contributors = require('github-contributors');
+var stringify = require('stringify-author');
+var reduce = require('async-array-reduce');
+var GitHub = require('github-base');
 
-module.exports = function (app, base, env) {
-  // var logger = app.logger('update-contributors');
+module.exports = function(app) {
+  var github;
 
-  // app.task('default', function (cb) {
-  //   try {
-  //     var pkg = base.getFile('package.json');
-  //     var data = extend({}, pkg.json);
-  //     // logger('done');
-  //     update(data, function (err, results) {
-  //       if (err) return cb(err);
-  //     console.log(results)
-  //       pkg.json = results;
-  //       cb();
-  //     });
-  //   } catch (err) {
-  //     cb(err);
-  //   }
-  // });
+  app.task('authenticate', function(cb) {
+    github = new GitHub();
+    // todo
+    cb();
+  });
+
+  app.task('default', ['authenticate'], function(cb) {
+    var data = app.pkg.expand();
+
+    contributors(data.repo, function(err, arr) {
+      if (err) return cb(err);
+
+      if (!Array.isArray(arr) || arr.length === 1) {
+        cb();
+        return;
+      }
+      users(arr, function(err, res) {
+        if (err) return cb(err);
+        app.pkg.set('contributors', res);
+        app.pkg.save();
+        cb();
+      });
+    });
+  });
+
+  function users(arr, cb) {
+    reduce(arr, [], function(acc, user, next) {
+      github.get(`/users/${user.login}`, function(err, res) {
+        if (err) return next(err);
+        var contributor = {name: res.name};
+        var url = res.blog || res.html_url
+        var email = res.email;
+        if (email) contributor.email = email;
+        if (url) contributor.url = url;
+        acc.push(stringify(contributor));
+        next(null, acc);
+      });
+    }, cb);
+  }
 };
+
